87aad56db69a3691dc36e45493dcc76bb7156c5e
ella <ejgsimmons@outlook.com>
2026-04-23 14:53:46 +0100

    update list to show calories

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 const fruitForm = document.querySelector("#inputSection form")
 const fruitList = document.querySelector("#fruitSection ul")
+const fruitNutrition = document.querySelector("#nutritionSection p")
 
 fruitForm.addEventListener("submit", extractFruit)
 
+let calories = 0
+
 function extractFruit(e){
     e.preventDefault(),
-    addFruit(e.target[0].value)
+    fetchFruitData(e.target[0].value)
     e.target[0].value="" 
+}
+
+function fetchFruitData(fruit){
+    fetch (`https://fruit-api-5v0j.onrender.com/fruits/${fruit}`)
+        .then((response)=>response.json())
+        .then((data)=>addFruit(data))
+        .catch((error)=>console.log(error))
 }
 
 function addFruit(fruit){
     const li = document.createElement("li")
-    li.textContent = fruit
+    li.textContent = fruit.name
+    li.dataset.calories = fruit.nutritions.calories
     li.addEventListener("click", removeFruit,{once: true})
     fruitList.appendChild(li)
+
+    calories += fruit.nutritions.calories
+    fruitNutrition.textContent = calories
 }
 
 function removeFruit(e){
+    calories -= e.target.dataset.calories
+    fruitNutrition.textContent = calories
     e.target.remove()
 }
+
