3231cf3ca19a58312283642a328c4da47d62c309
ella <ejgsimmons@outlook.com>
2026-04-23 18:55:39 +0100

    add images and edit error handling

--- script.js
+++ script.js
@@ -12,34 +12,37 @@ function extractFruit(e){
     e.target[0].value="" 
 }
 
-function fetchFruitData(fruit){
+async function fetchFruitData(fruit){
     try{
         const response = await fetch (`https://fruit-api-5v0j.onrender.com/fruits/${fruit}`)
-        const data = await response.json
-        addFruit(data)
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json()
+        const picresponse = await fetch(`https://pixabay.com/api/?key=55564662-2cfeda22c6b5119ce5f7ce3ba&q=${fruit}+fruit&image_type=photo`)
+        const picdata = await picresponse.json()
+        addFruit(data, picdata.hits[0]?.webformatURL)
     }   catch(error){
-        console.log(error)
+        console.log(`HTTP error! status: ${error}`)
     }
-    
-    //fetch (`${fruit}+fruit&category=food`)
-        // .then(processResponse)
-        // .then((pic)=> console.log(pic.hits.previewURL))
-        // .catch((error)=>console.log(error))
+   
 }
 
-function processResponse(response){
-    if (response.status === 200){
-        return response.json()
-    } else {
-        throw "Error: http status code" + response.status
-    }
-}
-
-function addFruit(fruit){
+function addFruit(fruit, imageUrl){
     const li = document.createElement("li")
     li.textContent = fruit.name
     li.dataset.calories = fruit.nutritions.calories
     li.addEventListener("click", removeFruit,{once: true})
+    
+    if(imageUrl){
+        const img = document.createElement("img")
+        img.src = imageUrl
+        img.alt = fruit.name
+        img.style.width = "100px"
+        img.style.marginLeft = "20px"
+        li.append(img)
+    }
+    
     fruitList.appendChild(li)
 
     calories += fruit.nutritions.calories
